0773437f4cfbd26e25f6f8371a8c34766f13c61a
Nishant Lanjewar <nishantlanjewar@Nishants-MacBook-Air.local>
2021-06-14 23:16:35 -0700

    defined state for cartItem component;

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,21 +1,39 @@
 import React from 'react';
 
 class CartItem extends React.Component{
+
+    constructor(){
+        super();
+        this.state={
+            price:999,
+            tital:'phone',
+            qty:1,
+            img:''
+        }
+    }
+
+    increaseQuentity=()=>{
+        console.log(this.state);
+    }
+    decreaseQuentity=()=>{
+        console.log('decreasing');
+    }
+
     render(){
+        const{qty,tital,price}=this.state;
         return(
             <div className='cart-item'>
                 <div className="left-block">
                     <img style={style.image}/>
                 </div>
-
                 <div className="right-block">
-                   <div style={{fontSize:25}}>Phone</div> 
-                   <div style={{color:'#777'}}>Rs 999</div> 
-                   <div style={{color:'#777'}}>Qty: 1</div>
+                   <div style={{fontSize:25}}>{tital}</div> 
+                   <div style={{color:'#777'}}>Rs {price}</div> 
+                   <div style={{color:'#777'}}>Qty: {qty}</div>
                    <div className="cart-item-actions">
-                        <div className="action-icons"></div> 
-                        <div className="action-icons"></div> 
-                        <div className="action-icons"></div> 
+                       <img alt="increase" className="action-icons" src="https://image.flaticon.com/icons/png/512/992/992651.png" onClick={this.increaseQuentity}/>
+                       <img alt="decrease" className="action-icons" src="https://image.flaticon.com/icons/png/512/992/992683.png" onClick={this.decreaseQuentity}/>
+                       <img alt="delete" className="action-icons" src="https://image.flaticon.com/icons/png/512/1214/1214428.png"/>
                    </div> 
                 </div>
 
